0d0dd9270d69df571fb92dda17ade1dca007176a
Vito9009 <yan.vi193@gmail.com>
2021-12-03 16:43:59 +0100

    calcolo-prezzo-biglietto-ok

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+let userInputKm = prompt ("Quanti chilometri vuoi percorrere?");
+let userInputAge = prompt ("Quanti anni ha il passeggero?");
+let pricePerKm = 0.21;
+
+let ticketPrice = userInputKm * pricePerKm;
+
+if(userInputAge < 18) {
+    document.getElementById("finalTicketPrice").innerHTML = 
+    ticketPrice - ((ticketPrice * 20) / 100) .toFixed(2) + "€";
+}else if(userInputAge > 65) {
+    document.getElementById("finalTicketPrice").innerHTML = 
+    ticketPrice - ((ticketPrice * 40) / 100) .toFixed(2) + "€";
+}else {
+    document.getElementById("finalTicketPrice").innerHTML = ticketPrice .toFixed(2);
+}
