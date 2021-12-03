4462a930e9f09c4dd768b0d1e390e36aee348ce9
Vito9009 <yan.vi193@gmail.com>
2021-12-03 17:40:37 +0100

    fix-"toFixed"-e-aggiunta-css

--- js/myscript.js
+++ js/myscript.js
@@ -6,10 +6,10 @@ let ticketPrice = userInputKm * pricePerKm;
 
 if(userInputAge < 18) {
     document.getElementById("finalTicketPrice").innerHTML = 
-    ticketPrice - ((ticketPrice * 20) / 100) .toFixed(2) + "€";
+    (ticketPrice - ((ticketPrice * 20) / 100)) .toFixed(2) + "€";
 }else if(userInputAge > 65) {
     document.getElementById("finalTicketPrice").innerHTML = 
-    ticketPrice - ((ticketPrice * 40) / 100) .toFixed(2) + "€";
+    (ticketPrice - ((ticketPrice * 40) / 100)) .toFixed(2) + "€";
 }else {
-    document.getElementById("finalTicketPrice").innerHTML = ticketPrice .toFixed(2);
+    document.getElementById("finalTicketPrice").innerHTML = ticketPrice .toFixed(2) + "€";
 }
